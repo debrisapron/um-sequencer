cc32abfac578df9baebbe8ea75ee2f46d229e1d0
DEBRIS APRON <debrisapron@gmail.com>
2018-02-23 18:12:34 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# um-sequencer
+
+A simple and generic JavaScript music sequencer with no dependencies.
+
+## Installation
+
+`yarn add um-sequencer` or `npm install um-sequencer --save`
+
+## Example
+
+```
+import Sequencer from 'um-sequencer'
+
+let audioContext = new window.AudioContext()
+let sequencer = Sequencer(() => audioContext.currentTime)
+
+sequencer.play([
+  { time: 0, callback: Beep(440) },
+  { time: 1, callback: Beep(440) },
+  { time: 2, callback: Beep(440) },
+  { time: 3, callback: Beep(880) }
+], {
+  tempo: 140,
+  loopLength: 4
+})
+
+setTimeout(sequencer.stop, 5000)
+
+function Beep(freq) {
+  return (time) => {
+    let osc = audioContext.createOscillator()
+    osc.frequency.value = freq
+    osc.connect(audioContext.destination)
+    osc.start(time)
+    osc.stop(time + 0.1)
+  }
+}
+```
+
+## API
+
+### Sequencer(getCurrentTime, [opts])
+
+Creates a sequencer object with the given clock source & timing options.
+
+`getCurrentTime` is a function which should return the current time in seconds. The most typical source for this would be the Web Audio API `AudioContext` `currentTime` property, but it could be any high-resolution clock.
+
+`opts` is an optional config object with the attributes `interval`, to set the time between clock ticks, and `lookahead`, to set the window of time after a tick in which found events will be scheduled. If you do not understand these parameters, don't mess with them. For more information see [A Tale Of Two Clocks](https://www.html5rocks.com/en/tutorials/audio/scheduling/).
+
+### sequencer.play(events, [opts])
+
+Begins playback of a sequence of events with the given options.
+
+`events` is an array of event objects each of which should have two attributes, `time`, the time in quarter-notes from the beginning of the sequence at which the event should fire, and `callback`, the function to be called at that time.
+
+`opts` is an optional config object with the attributes `tempo`, the tempo in bpm at which to play the sequence, and `loopLength`, a length in quarter-notes at which to loop the sequence. If no `loopLength` is specified, the sequence will not loop.
+
+### sequencer.stop()
+
+Stops playback of the sequence.
+
+## TODO
+
+- Tests
+- Change tempo during playback
+- Optionally run timer in a web worker
+- Handle changing sequence during playback (same length)
+- Handle changing sequence during playback (different length)
---
+++ index.js
@@ -0,0 +1,99 @@
+function Sequencer(getCurrentTime, opts = {}) {
+
+  // NOTE
+  // All absolute times are in seconds.
+  // All musical times are in quarter-notes.
+
+  let _interval = opts.interval || 0.025 // Time between ticks.
+  let _lookahead = opts.lookahead || 0.1 // Time to look ahead for events to schedule.
+  let _tempo
+  let _timerId
+  let _nextEventIndex
+  let _nextEventTime
+  let _events
+  let _deltas
+
+  function dispatch() {
+    let callback = _events[_nextEventIndex].callback
+    if (callback) { callback(_nextEventTime) }
+  }
+
+  // Move the pointer to the next note.
+  function advance() {
+    let loop = _events[_nextEventIndex].loop
+    let isLastEvent = _nextEventIndex === _deltas.length - 1
+
+    // If we are not looping and this is the end of the sequence, stop.
+    if (isLastEvent && !loop) {
+      stop()
+      return
+    }
+
+    // If we are at the loop point, move it to the first note.
+    _nextEventIndex = loop ? 0 : _nextEventIndex + 1
+    _nextEventTime += secsFromQuarterNotes(_deltas[_nextEventIndex])
+  }
+
+  // While there are notes that will need to play during the next lookahead period,
+  // schedule them and advance the pointer.
+  function onTick() {
+    let horizon = getCurrentTime() + _lookahead
+    while (isPlaying() && (_nextEventTime < horizon)) {
+      dispatch()
+      advance()
+    }
+  }
+
+  function startClock() {
+    _timerId = setInterval(onTick, _interval * 1000)
+  }
+
+  function secsFromQuarterNotes(qns) {
+    return qns * (60 / _tempo)
+  }
+
+  function init(events, opts) {
+    _tempo = opts.tempo || 120
+
+    // Add the loop event if present & sort the events by time.
+    _events = events.slice()
+    if (opts.loopLength) {
+      _events.push({ time: opts.loopLength, loop: true })
+    }
+    _events.sort((a, b) => a.time - b.time)
+
+    // For each event, get the delta time since the previous event.
+    _deltas = _events.map(({ time, callback }, i, arr) => {
+      return i === 0 ? time : (time - arr[i - 1].time)
+    })
+
+    // Point the sequencer to the first event.
+    _nextEventIndex = 0
+
+    // Schedule the first event to play after a tick has passed.
+    _nextEventTime = getCurrentTime() + _interval + secsFromQuarterNotes(_deltas[0])
+  }
+
+  function isPlaying() {
+    return !!_timerId
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  function play(events, opts = {}) {
+    if (isPlaying()) { stop() }
+    init(events, opts)
+    startClock()
+  }
+
+  function stop() {
+    if (_timerId) {
+      clearInterval(_timerId)
+      _timerId = null
+    }
+  }
+
+  return { play, stop }
+}
+
+export default Sequencer
